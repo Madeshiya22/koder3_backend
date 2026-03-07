d1f4a15f41f0a9b18e441d959e50b4cdbd2f9642
Rahul Madeshiya <rahulmadeshiya851@gmail.com>
2026-03-07 08:48:53 +0530

    changing something

---
+++ Day-2/server.js
@@ -0,0 +1,46 @@
+import express from "express";
+// import morgan from "morgan";
+
+const app = express();
+
+app.use(express.json());
+
+// app.use(morgan("dev"));
+
+const notes = [];
+
+app.post("/notes", (req, res) => {
+  notes.push(req.body);
+  console.log(notes);
+  res.status(201).json({ message: "Note created successfully" });
+});
+
+app.get("/notes", (req, res) => {
+  res.status(200).json({
+    message: "Note created successfully",
+    notes,
+  });
+});
+
+app.delete("/notes/:index", (req, res) => {
+  const index = req.params.index;
+  delete notes[index];
+  res.status(200).json({ message: "Note deleted successfully" });
+});
+
+app.patch("/notes/:index", (req, res) => {
+  const index = req.params.index;
+  const { description } = req.body;
+
+  notes[index].description = description;
+
+  res.status(200).json({
+     message: "Note updated successfully"
+     });
+});
+
+
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
